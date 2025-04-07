6761cc6babd1093f85db92f9dd8b843cfea01911
marcin <marcin@siudzinski.eu>
2025-04-07 21:50:28 +0200

    add init for airflow lab

--- dbt/lab-dbt01/startup.sh
+++ dbt/lab-dbt01/startup.sh
@@ -73,5 +73,5 @@ echo "Startup script completed." >> /mnt/data/log/lab-startup.log
 date >> /mnt/data/log/lab-startup.log
 
 docker pull siudzinskim/vscode-dbt
-docker run -d  -p 8888:8443 -v /mnt/data/vscode:/config --name vs siudzinskim/vscode-dbt
+docker run -d  -p 8888:8443 -p 8080:8080 -v /mnt/data/vscode:/config --name vs siudzinskim/vscode-dbt
 (crontab -l ; echo "@reboot /usr/bin/docker start vs") | crontab -
--- dbt/lab-dbt05/init.sh
+++ dbt/lab-dbt05/init.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 mkdir -p /config/airflow/
+cp airflow.cfg /config/airflow/airflow.cfg
 cp webserver_config.py /config/airflow/webserver_config.py
