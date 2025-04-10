1cba3c0448d6f1089926a2b31e03289c531c623d
marcin <marcin@siudzinski.eu>
2025-04-10 15:14:20 +0200

    make airflow working

---
+++ dbt/lab-dbt05/airflow/init.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /config/airflow/dags/
+cp airflow.cfg /config/airflow/airflow.cfg
+cp webserver_config.py /config/airflow/webserver_config.py
+cp -R dags/* /config/airflow/dags
